2d15ebfdca13acbf29bca493c47ec0f93904c1f8
four.in.space <renofourhi@gmail.com>
2025-11-25 13:57:29 +0900

    Update images.js

--- images.js
+++ images.js
@@ -1,155 +1,180 @@
 // List of images/videos/GIFs
 const images = [
-   {src: "https://lh3.googleusercontent.com/d/10LwSeuG7sroVKfqwUuLMffJcnFifg8pt=w1200",  
-    alt: "DTA App",
-    categories: ["product design", "digital"],
-    group: [
-      "https://lh3.googleusercontent.com/d/1j5qKxY-NJVM0dgIREv00jfPGrFuFK2Ey=w1200",
-       "https://lh3.googleusercontent.com/d/1b01BD7GA7y1bbSf_aWEnE8WTX8aaG6xm=w1200"
-     ],
-  text: "This is the custom text that will appear in the lightbox."
+{src: "https://lh3.googleusercontent.com/d/10LwSeuG7sroVKfqwUuLMffJcnFifg8pt=w1200",  
+      alt: "DTA App",
+      categories: ["product design", "digital"],
+      group: [ "https://lh3.googleusercontent.com/d/1j5qKxY-NJVM0dgIREv00jfPGrFuFK2Ey=w1200",
+               "https://lh3.googleusercontent.com/d/1b01BD7GA7y1bbSf_aWEnE8WTX8aaG6xm=w1200" ],
+  text: "1 - This is the custom text that will appear in the lightbox."
   },
     
-   { src: "https://lh3.googleusercontent.com/d/1It6_fexSflYCfj_h6NhbUvr6yZZkVF2Y=w1200",
-    alt: "MAU",
-    categories: ["digital"]
+{ src: "https://lh3.googleusercontent.com/d/1It6_fexSflYCfj_h6NhbUvr6yZZkVF2Y=w1200",
+       alt: "MAU",
+       categories: ["digital"],
+       group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+       text: "2 - This is the custom text that will appear in the lightbox."
     
 },
  { src: "https://lh3.googleusercontent.com/d/1PvqBPlD0JysbDoav79Zoa0SeXRkVCH95=w1200",
-  alt: "Think", 
-  categories: ["print", "digital"]
+        alt: "Think", 
+        categories: ["print", "digital"],
+        group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+        text: "3 - This is the custom text that will appear in the lightbox."
 },
 
  { src: "https://lh3.googleusercontent.com/d/1-IfFsOem4_uFoWHL7x0gUz-Nkk8s-Bu8=w1200", 
-    alt: "30DII",
-    categories: ["print"],
-    group: [
-      "https://lh3.googleusercontent.com/d/15FTIUfeVcqKgX-D7WNouuUOAzcj-2VRC=w1200",
-      "https://lh3.googleusercontent.com/d/1kEALBwWjwUwCIXXtLOPIFRjghW6zi3gD=w1200",
-      "https://lh3.googleusercontent.com/d/1eGDP41pcqUYDwj1IMc4LDK_IY5lMxOUj=w1200",
-      "https://lh3.googleusercontent.com/d/1o9Yoe_mLvZtMifItUwLA02WHEtrz-oss=w1200",
-      "https://lh3.googleusercontent.com/d/1FZkyoX1s0bDIRWeEHrgwfNtbylAyjZzS=w1200",
-      "https://lh3.googleusercontent.com/d/1rbu6THSuW3DzBIBJXI0MmVY48Zpjsbjy=w1200",
-      "https://lh3.googleusercontent.com/d/1ky8YgZzDVj4IVouvlf8YLbRjOJimsqn-=w1200",
-      "https://lh3.googleusercontent.com/d/1hkt52J-1Ec2temdDGcoGBzZaicaoiO2n=w1200"
-    ]
+         alt: "30DII",
+         categories: ["print"],
+         group: [ "https://lh3.googleusercontent.com/d/15FTIUfeVcqKgX-D7WNouuUOAzcj-2VRC=w1200",
+                  "https://lh3.googleusercontent.com/d/1kEALBwWjwUwCIXXtLOPIFRjghW6zi3gD=w1200",
+                  "https://lh3.googleusercontent.com/d/1eGDP41pcqUYDwj1IMc4LDK_IY5lMxOUj=w1200",
+                  "https://lh3.googleusercontent.com/d/1o9Yoe_mLvZtMifItUwLA02WHEtrz-oss=w1200",
+                  "https://lh3.googleusercontent.com/d/1FZkyoX1s0bDIRWeEHrgwfNtbylAyjZzS=w1200",
+                  "https://lh3.googleusercontent.com/d/1rbu6THSuW3DzBIBJXI0MmVY48Zpjsbjy=w1200",
+                  "https://lh3.googleusercontent.com/d/1ky8YgZzDVj4IVouvlf8YLbRjOJimsqn-=w1200",
+                  "https://lh3.googleusercontent.com/d/1hkt52J-1Ec2temdDGcoGBzZaicaoiO2n=w1200" ],
+        text: "4- This is the custom text that will appear in the lightbox."
 },
   { src: "https://lh3.googleusercontent.com/d/1NhuBTVYigNda4gX7SjaG1AnYxWhwlfoe=w1200",
-  alt: "Space Invaders",
-  categories: ["print", "digital"]
+         alt: "Space Invaders",
+         categories: ["print", "digital"],
+         group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+         text: "5 - This is the custom text that will appear in the lightbox."
 },
   
   { src: "https://lh3.googleusercontent.com/d/1PXKgsT4wJTHYtTM6AEO2JrviddGzrMlH=w1200", 
-   alt: "Flags", 
-   categories: ["3d"], 
-   group: [
-     "https://lh3.googleusercontent.com/d/1z5hlLZvZ7E89QKkdwBOegW3u45UeAJqx=w1200",
-     "https://lh3.googleusercontent.com/d/1Q6AyJgcSu3-xw_O13a8bnkl_QyzhelBz=w1200",
-     "https://lh3.googleusercontent.com/d/1mMb37RFB4RQGqMT5fSKAE4F4wnL8RpW1=w1200",
-     "https://lh3.googleusercontent.com/d/1v-qeoTEP0I1Vq0XCyH_FyVOKF4ILfqfR=w1200",
-     "https://lh3.googleusercontent.com/d/14e7R4cbk4nc7pmW5EXUKt16GlX3auofQ=w1200",
-     "https://lh3.googleusercontent.com/d/1rHPUDFseJP4AEG13pNXGXgOvaN-zBWmx=w1200"
-    ]
+          alt: "Flags", 
+          categories: ["3d"], 
+          group: [ "https://lh3.googleusercontent.com/d/1z5hlLZvZ7E89QKkdwBOegW3u45UeAJqx=w1200",
+                   "https://lh3.googleusercontent.com/d/1Q6AyJgcSu3-xw_O13a8bnkl_QyzhelBz=w1200",
+                   "https://lh3.googleusercontent.com/d/1mMb37RFB4RQGqMT5fSKAE4F4wnL8RpW1=w1200",
+                   "https://lh3.googleusercontent.com/d/1v-qeoTEP0I1Vq0XCyH_FyVOKF4ILfqfR=w1200",
+                   "https://lh3.googleusercontent.com/d/14e7R4cbk4nc7pmW5EXUKt16GlX3auofQ=w1200",
+                   "https://lh3.googleusercontent.com/d/1rHPUDFseJP4AEG13pNXGXgOvaN-zBWmx=w1200" ],
+        text: "6 - This is the custom text that will appear in the lightbox."
   },
   
-{
-  src: "https://lh3.googleusercontent.com/d/1SO1zNKqRqplLpv-RzrRggZBUPDRL3FvG=w1200",
-  alt: "PLAUAT App",
-  categories: ["product design", "digital"],
+{ src: "https://lh3.googleusercontent.com/d/1SO1zNKqRqplLpv-RzrRggZBUPDRL3FvG=w1200",
+       alt: "PLAUAT App",
+       categories: ["product design", "digital"],
+       group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+       text: "7 - This is the custom text that will appear in the lightbox."
 },
 
 
 
   { src: "https://lh3.googleusercontent.com/d/1ImWqmE7xA0CBkEe3uJnudWVErv_zBnCP=w1200", 
-   alt: "Taste", 
-   categories: ["print", "digital"] 
+         alt: "Taste", 
+         categories: ["print", "digital"],
+         group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+         text: "8 - This is the custom text that will appear in the lightbox."
   },
  
-  { src: "https://lh3.googleusercontent.com/d/1I7S1LB7e2FaIMDz9Zz6Fm5Ipwug2uqmF=w1200", alt: "Kiiroo-2", categories: ["product"] },
+  { src: "https://lh3.googleusercontent.com/d/1I7S1LB7e2FaIMDz9Zz6Fm5Ipwug2uqmF=w1200", 
+         alt: "Kiiroo", 
+         categories: ["product"],
+         group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+         text: "9 - This is the custom text that will appear in the lightbox."
+  },
   { src: "https://lh3.googleusercontent.com/d/1AdUvGtMf8am8vdEvKlYeGbiECrhO24KA=w1200",
-  alt: "Glass_toooth",
-  categories: ["digital"]
+         alt: "Glass_toooth",
+         categories: ["digital"],
+         group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+         text: "10 - This is the custom text that will appear in the lightbox."
 },
   
 { src: "https://lh3.googleusercontent.com/d/1LHJU0gas_N5T2cZbtnIcpU3ySLtd6drf=w1200", 
-  alt: "Alien", 
-  categories: ["digital"] 
+       alt: "Jerrican", 
+       categories: ["print", "digital"],
+       group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+       text: "11 - This is the custom text that will appear in the lightbox."
 },
   { src: "https://lh3.googleusercontent.com/d/1eHsnGo3XG8R9-gItEXHnWNNzkwf4M9Gy=w1200",
-  alt: "Rings",
-  categories: ["digital"]
+         alt: "Rings",
+         categories: ["digital"],
+         group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+         text: "12 - This is the custom text that will appear in the lightbox."
 }, 
 
   { src: "https://lh3.googleusercontent.com/d/1OEubZ1zimJZ9ozsBWBDxrglqhQyElMQd=w1200",
-  alt: "Nike",
-  categories: ["digital"]
+         alt: "Nike",
+         categories: ["digital"],
+         group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+         text: "13 - This is the custom text that will appear in the lightbox."
    },
  
 
 { src: "https://lh3.googleusercontent.com/d/1jt1a1GyDPq9OeYrgfxcVIXMTK3md0Sbl=w1200",
-  alt: "Next Rise",
-  categories: ["product design", "digital"],
+        alt: "Next Rise",
+        categories: ["product design", "digital"],
+        group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+        text: "14 - This is the custom text that will appear in the lightbox."
 },
 
  
 { src: "https://lh3.googleusercontent.com/d/1H8_lRyafliGnAOtn20LoAbxz8h3Ztz0n=w1200",
-  alt: "Artinus",
-  categories: ["digital"]
+       alt: "Artinus",
+       categories: ["digital"],
+       group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+       text: "15 - This is the custom text that will appear in the lightbox."
 },
   
-{
-  src: "https://lh3.googleusercontent.com/d/1c-H35RgNXWWiEODJMmWdXJYiuzSNBZVx=w1200",
-  alt: "Sit App",
-  categories: ["product design", "digital"],
-  group: [
-      "https://lh3.googleusercontent.com/d/1LwfVVH0btkOtS2m9MvfRTa4UFudwmLuu=w1200"
-  ],
-  text: "SIT IS A PEER-TO-PEER MOBILE APPLICATION FOR HIGH-DENSITY, UNRESERVED TRANSIT. IT ALLOWS EXITING PASSENGERS TO LIST THEIR HIGHLY DESIRED SEAT IN REAL-TIME. STANDING RIDERS CAN PURCHASE THE IMMEDIATE RIGHT OF OCCUPANCY SECURELY VIA THE APP. USING PRECISE LOCATION AND TIME TRACKING, SIT COORDINATES THE EXCHANGE.
-ELIMINATE THE STRESS OF STANDING AND GUARANTEE COMFORT FOR THE FINAL LEG. SECURE, DISCREET PAYMENT ENSURES A SEAMLESS TRANSITION BETWEEN RIDERS. SIT TURNS AN OCCUPIED SPOT INTO A GUARANTEED, PURCHASABLE SEAT.
-"
+{ src: "https://lh3.googleusercontent.com/d/1c-H35RgNXWWiEODJMmWdXJYiuzSNBZVx=w1200",
+       alt: "Sit App",
+       categories: ["product design", "digital"],
+       group: [ "https://lh3.googleusercontent.com/d/1LwfVVH0btkOtS2m9MvfRTa4UFudwmLuu=w1200" ],
+       text: "16 - This is the custom text that will appear in the lightbox."
   },
-   {
-  src: "https://lh3.googleusercontent.com/d/1TK4f5gCsP7BFOEADC_S95nVC12_NWpZj=w1200",
-  alt: "Polyspot",
-  categories: ["product design", "digital"],
-  group: [
-       "https://lh3.googleusercontent.com/d/1qnW1cFUEsYKX1RdIM5KOKpBahhViYXez=w1200",
-       "https://lh3.googleusercontent.com/d/1s_MAOrC2y8wTnnRGB-eUiq2uy1eQVCNa=w1200",
-       "https://lh3.googleusercontent.com/d/1L7bKdukUgHA5o4YoOIjUcXXyLxxz73hF=w1200",
-       "https://lh3.googleusercontent.com/d/1bib0w_76GibZlfhs1QqmXeViudPwg710=w1200"
-  ]
+   
+{ src: "https://lh3.googleusercontent.com/d/1TK4f5gCsP7BFOEADC_S95nVC12_NWpZj=w1200",
+       alt: "Polyspot",
+       categories: ["product design", "digital"],
+       group: [ "https://lh3.googleusercontent.com/d/1qnW1cFUEsYKX1RdIM5KOKpBahhViYXez=w1200",
+                "https://lh3.googleusercontent.com/d/1s_MAOrC2y8wTnnRGB-eUiq2uy1eQVCNa=w1200",
+                "https://lh3.googleusercontent.com/d/1L7bKdukUgHA5o4YoOIjUcXXyLxxz73hF=w1200",
+                "https://lh3.googleusercontent.com/d/1bib0w_76GibZlfhs1QqmXeViudPwg710=w1200" ],
+       text: "17 - This is the custom text that will appear in the lightbox."
   },
  
  { src: "https://lh3.googleusercontent.com/d/12yh7OmQPdnBEHswGDG6-6HqUXB12-zOM=w1200", 
-    alt: "Smile", 
-    categories: ["digital"], 
-    group: [
-      "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200",
-      "https://lh3.googleusercontent.com/d/1kjSC1Cq3ZJyi6uBe1CF1yWqAla9l_-d8=w1200",
-      "https://lh3.googleusercontent.com/d/1cGf6mqDp5e4s6QV0IZwA7nvfin1FuDOi=w1200",
-      "https://lh3.googleusercontent.com/d/1FEBL8nfwI6VgErRb95TB0B0vGjB7Jxu2=w1200",
-      "https://lh3.googleusercontent.com/d/1MHcfbajeA9-m3nAckdwFLcxsG-HwFz4r=w1200",
-      "https://lh3.googleusercontent.com/d/1pqqaSgQbtqtSytEhxJzlrJA0hgFDWbvt=w1200"
-  ]
+        alt: "Smile", 
+        categories: ["digital"], 
+        group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200",
+                 "https://lh3.googleusercontent.com/d/1kjSC1Cq3ZJyi6uBe1CF1yWqAla9l_-d8=w1200",
+                 "https://lh3.googleusercontent.com/d/1cGf6mqDp5e4s6QV0IZwA7nvfin1FuDOi=w1200",
+                 "https://lh3.googleusercontent.com/d/1FEBL8nfwI6VgErRb95TB0B0vGjB7Jxu2=w1200",
+                 "https://lh3.googleusercontent.com/d/1MHcfbajeA9-m3nAckdwFLcxsG-HwFz4r=w1200",
+                 "https://lh3.googleusercontent.com/d/1pqqaSgQbtqtSytEhxJzlrJA0hgFDWbvt=w1200" ],
+         text: "18 - This is the custom text that will appear in the lightbox."
   },
 
   
- { src: "https://lh3.googleusercontent.com/d/1j9CGeOUhwHnYE1JNWCfQlpXl1ATJZWTh=w1200", 
-  alt: "Bikeride", 
-  categories: ["digital"] 
+{ src: "https://lh3.googleusercontent.com/d/1j9CGeOUhwHnYE1JNWCfQlpXl1ATJZWTh=w1200", 
+        alt: "Bikeride", 
+        categories: ["digital"], 
+        group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+        text: "19 - This is the custom text that will appear in the lightbox."
 }, 
 
  { src: "https://lh3.googleusercontent.com/d/1YcTTIG9o4bVfpRpZZuYYuSYrFLfWHCUb=w1200",
-  alt: "Prompt",
-  categories: ["digital"]
+        alt: "Prompt",
+        categories: ["digital"],
+        group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+        text: "20 - This is the custom text that will appear in the lightbox."
 },
  
-  { src: "https://lh3.googleusercontent.com/d/1citj8Zu7j3JEzF-B7kczQiYVg8n-PNqo=w1200", alt: "Stella", categories: ["print", "digital"] 
+  { src: "https://lh3.googleusercontent.com/d/1citj8Zu7j3JEzF-B7kczQiYVg8n-PNqo=w1200", 
+         alt: "Stella", 
+         categories: ["print", "digital"] ,
+         group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+         text: "21 - This is the custom text that will appear in the lightbox."
   },
    
-   {  src: "https://lh3.googleusercontent.com/d/1YkYFncv1MhxGUAwpY1LcoO-XM__HQp1D=w1200",
-  alt: "Ballsy",
-  categories: ["product design", "digital"]
+{ src: "https://lh3.googleusercontent.com/d/1YkYFncv1MhxGUAwpY1LcoO-XM__HQp1D=w1200",
+      alt: "Ballsy",
+      categories: ["product design", "digital"],
+      group: [ "https://lh3.googleusercontent.com/d/1bv1-8DTuKQjDJV3ixN6IOK9CrP68qg79=w1200" ],
+      text: "22 - This is the custom text that will appear in the lightbox."
 },
 ];
 
